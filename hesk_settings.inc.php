1513e23a9d1b22874accec5e616ee16601893482
marcuspgs <marcuspgs@msn.com>
2025-09-17 13:47:48 -0300

    update3.6.3

--- hesk_settings.inc.php
+++ hesk_settings.inc.php
@@ -1,5 +1,5 @@
 <?php
-// Settings file for HESK 3.6.2
+// Settings file for HESK 3.6.3
 
 // ==> GENERAL
 
@@ -262,7 +262,7 @@ $hesk_settings['check_updates']=1;
 #############################
 #     DO NOT EDIT BELOW     #
 #############################
-$hesk_settings['hesk_version']='3.6.2';
+$hesk_settings['hesk_version']='3.6.3';
 if ($hesk_settings['debug_mode'])
 {
     error_reporting(E_ALL);
--- install/install_functions.inc.php
+++ install/install_functions.inc.php
@@ -15,7 +15,7 @@
 if (!defined('IN_SCRIPT')) {die('Invalid attempt');}
 
 // We will be installing this HESK version:
-define('HESK_NEW_VERSION','3.6.2');
+define('HESK_NEW_VERSION','3.6.3');
 define('REQUIRE_PHP_VERSION','5.6.0');
 define('REQUIRE_MYSQL_VERSION','5.0.7');
 
